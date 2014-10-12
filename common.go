da39db8c9bd0377d0f7a5a049120daf9c744b4ac
Brian Downs <brian.downs@gmail.com>
2014-10-12 15:28:55 -0700

    fix str format when using float64

--- common.go
+++ common.go
@@ -3,5 +3,5 @@ package openweathermap
 const (
 	baseUrl  string = "http://api.openweathermap.org/data/2.5/weather?%s"
 	cityUrl  string = "http://api.openweathermap.org/data/2.5/weather?q=%s"
-	coordUrl string = "http://api.openweathermap.org/data/2.5/weather?lat=%s&long=%s"
+	coordUrl string = "http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f"
 )
