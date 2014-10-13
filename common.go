5296ac3e2f93dd75e742ec3432c942c14d486071
Brian Downs <brian.downs@gmail.com>
2014-10-12 20:24:29 -0700

    moved structs and methods to common

--- common.go
+++ common.go
@@ -1,1 +1,62 @@
 package openweathermap
+
+const (
+	baseUrl string = "http://api.openweathermap.org/data/2.5/weather?%s"
+)
+
+var (
+	dataUnits = [3]string{"metric", "imperial", "internal"}
+)
+
+type Coordinates struct {
+	Longitude float64 `json:"lon"`
+	Latitude  float64 `json:"lat"`
+}
+
+type Sys struct {
+	Type    int     `json:"type"`
+	Id      int     `json:"id"`
+	Message float64 `json:"message"`
+	Country string  `json:"country"`
+	Sunrise int     `json:"sunrise"`
+	Sunset  int     `json:"sunset"`
+}
+
+type Wind struct {
+	Speed float64 `json:"speed"`
+	Deg   int     `json:"deg"`
+}
+
+type Weather struct {
+	Id          int    `json:"id"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+
+type Main struct {
+	Temp     float64 `json:"temp"`
+	TempMin  float64 `json:"temp_min"`
+	TempMax  float64 `json:"temp_max"`
+	Pressure int     `json:"pressure"`
+	Humidity int     `json:"humidity"`
+}
+
+type Clouds struct {
+	All int `json:"all"`
+}
+
+type WeatherData struct {
+	GeoPos  Coordinates `json:"coord"`
+	Sys     Sys         `json:"sys"`
+	Base    string      `json:"base"`
+	Weather []Weather   `json:"weather"`
+	Main    Main        `json:"main"`
+	Wind    Wind        `json:"wind"`
+	Clouds  Clouds      `json:"clouds"`
+	Dt      int         `json:"dt"`
+	Id      int         `json:"id"`
+	Name    string      `json:"name"`
+	Cod     int         `json:"cod"`
+	Units   string
+}
