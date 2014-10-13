30c9f501573acdff7fe4d92d0354383e5f19992c
Brian Downs <brian.downs@gmail.com>
2014-10-13 08:10:50 -0700

    started on historical code

--- history.go
+++ history.go
@@ -1,1 +1,18 @@
 package openweathermap
+
+import (
+	"errors"
+	"strings"
+)
+
+// New returns a new HistoricalWeatherData pointer with the supplied
+// arguments.
+func New(unit string) (*HistoricalWeatherData, error) {
+	unitChoice := strings.ToLower(unit)
+	for _, i := range dataUnits {
+		if strings.Contains(unitChoice, i) {
+			return &HistoricalWeatherData{Units: unitChoice}, nil
+		}
+	}
+	return nil, errors.New("ERROR: unit of measure not available")
+}
