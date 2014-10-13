086d664a8a5f5594158c96227046cbf9d240a177
Brian Downs <brian.downs@gmail.com>
2014-10-13 08:08:43 -0700

    comment updates

--- common.go
+++ common.go
@@ -8,11 +8,16 @@ var (
 	dataUnits = [3]string{"metric", "imperial", "internal"}
 )
 
+// Coordinates struct holds longitude and latitude data
+// in returned JSON or as parameter data for requests
+// using longitude and latitude.
 type Coordinates struct {
 	Longitude float64 `json:"lon"`
 	Latitude  float64 `json:"lat"`
 }
 
+// Sys struct contains general information about the request and the
+// surrounding area for where the request was made.
 type Sys struct {
 	Type    int     `json:"type"`
 	Id      int     `json:"id"`
@@ -22,11 +27,14 @@ type Sys struct {
 	Sunset  int     `json:"sunset"`
 }
 
+// Wind struct contains the speed and degree of the wind.
 type Wind struct {
 	Speed float64 `json:"speed"`
 	Deg   int     `json:"deg"`
 }
 
+// Weather struct holds high-level basic info on the returned
+// data.
 type Weather struct {
 	Id          int    `json:"id"`
 	Main        string `json:"main"`
@@ -34,6 +42,7 @@ type Weather struct {
 	Icon        string `json:"icon"`
 }
 
+// Main struct contains the meat and potatos of the request.
 type Main struct {
 	Temp     float64 `json:"temp"`
 	TempMin  float64 `json:"temp_min"`
@@ -42,10 +51,13 @@ type Main struct {
 	Humidity int     `json:"humidity"`
 }
 
+// Clouds struct holds data regarding cloud cover.
 type Clouds struct {
 	All int `json:"all"`
 }
 
+// CurrentWeatherData struct contains an aggregate view of the structs
+// defined above for JSON to be unmarshaled into.
 type CurrentWeatherData struct {
 	GeoPos  Coordinates `json:"coord"`
 	Sys     Sys         `json:"sys"`
@@ -61,6 +73,8 @@ type CurrentWeatherData struct {
 	Units   string
 }
 
+// WeatherHistory struct contains aggregate fields from the above
+// structs.
 type WeatherHistory struct {
 	Main    Main      `json:"main"`
 	Wind    Wind      `json:"wind"`
@@ -69,6 +83,8 @@ type WeatherHistory struct {
 	Dt      int       `json:"dt"`
 }
 
+// HistoricalWeatherData struct is where the JSON is unmarshaled to
+// when receiving data for a historical request.
 type HistoricalWeatherData struct {
 	Message  string           `json:"message"`
 	Cod      int              `json:"cod"`
@@ -78,6 +94,8 @@ type HistoricalWeatherData struct {
 	List     []WeatherHistory `json:"list"`
 }
 
+// HistoricalParameters struct holds the (optional) fields to be
+// supplied for historical data requests.
 type HistoricalParameters struct {
 	Start int64
 	End   int64
--- doc.go
+++ doc.go
@@ -1,1 +1,4 @@
+// openweathermap is a library for use to collect data using the
+// http://openweathermap.org API.  Though XML is available via the
+// API, JSON is the only format supported at this time.
 package openweathermap
