b471a5eb934fac08ed512dffcdbcdec940200c0c
Brian Downs <brian.downs@gmail.com>
2016-01-15 10:16:13 -0700

    initial commit for branch

---
+++ ais.go
@@ -0,0 +1,19 @@
+package openweathermap
+
+// GetShipInfo returns current information about vessel specified by
+// MMSI. You will get detailed API respond if the vessel is in a list
+// of your registered vessels on 'My Fleet' page. You get limited
+// information for vessels that are not in a list of your account
+func GetShipInfo() {}
+
+// MyFleet
+func MyFleet() {}
+
+// ShipTrack
+func ShipTrack() {}
+
+// AddVessel
+func AddVessel() {}
+
+// RemoveVessel
+func RemoveVessel() {}
