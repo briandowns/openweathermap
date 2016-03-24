340719af22ac71ff18209a325549b6633eb88fc7
Nevkontakte <aleks@nevkontakte.com>
2016-03-24 22:42:20 +0000

    Add support for rain and snow in current weathr conditions

--- current.go
+++ current.go
@@ -25,17 +25,19 @@ import (
 // CurrentWeatherData struct contains an aggregate view of the structs
 // defined above for JSON to be unmarshaled into.
 type CurrentWeatherData struct {
-	GeoPos  Coordinates `json:"coord"`
-	Sys     Sys         `json:"sys"`
-	Base    string      `json:"base"`
-	Weather []Weather   `json:"weather"`
-	Main    Main        `json:"main"`
-	Wind    Wind        `json:"wind"`
-	Clouds  Clouds      `json:"clouds"`
-	Dt      int         `json:"dt"`
-	ID      int         `json:"id"`
-	Name    string      `json:"name"`
-	Cod     int         `json:"cod"`
+	GeoPos  Coordinates        `json:"coord"`
+	Sys     Sys                `json:"sys"`
+	Base    string             `json:"base"`
+	Weather []Weather          `json:"weather"`
+	Main    Main               `json:"main"`
+	Wind    Wind               `json:"wind"`
+	Clouds  Clouds             `json:"clouds"`
+	Rain    map[string]float64 `json:"rain"`
+	Snow    map[string]float64 `json:"snow"`
+	Dt      int                `json:"dt"`
+	ID      int                `json:"id"`
+	Name    string             `json:"name"`
+	Cod     int                `json:"cod"`
 	Unit    string
 	Lang    string
 	Key     string
