803493291df80e4b3a2287d8f4200bd4322be744
Andrea Manzini <ilmanzo@gmail.com>
2018-08-04 17:46:55 +0200

    updated cli example with forecast

--- _examples/cli/weather.go
+++ _examples/cli/weather.go
@@ -52,11 +52,22 @@ const weatherTemplate = `Current weather for {{.Name}}:
     Low:         {{.Main.TempMin}} {{.Unit}}
 `
 
+const forecastTemplate = `Weather Forecast for {{.City.Name}}:
+{{range .List}}Date & Time: {{.DtTxt}}
+Conditions:  {{range .Weather}}{{.Main}} {{.Description}}{{end}}
+Temp:        {{.Main.Temp}} 
+High:        {{.Main.TempMax}} 
+Low:         {{.Main.TempMin}}
+
+{{end}}
+`
+
 // Pointers to hold the contents of the flag args.
 var (
 	whereFlag = flag.String("w", "", "Location to get weather.  If location has a space, wrap the location in double quotes.")
 	unitFlag  = flag.String("u", "", "Unit of measure to display temps in")
 	langFlag  = flag.String("l", "", "Language to display temps in")
+	whenFlag  = flag.String("t", "current", "current | forecast")
 )
 
 // Data will hold the result of the query to get the IP
@@ -102,20 +113,29 @@ func getLocation() (*Data, error) {
 
 // getCurrent gets the current weather for the provided
 // location in the units provided.
-func getCurrent(l, u, lang string) (*owm.CurrentWeatherData, error) {
-	w, err := owm.NewCurrent(u, lang, os.Getenv("OWM_API_KEY"))
+func getCurrent(location, units, lang string) (*owm.CurrentWeatherData, error) {
+	w, err := owm.NewCurrent(units, lang, os.Getenv("OWM_API_KEY"))
 	if err != nil {
 		return nil, err
 	}
-	w.CurrentByName(l)
+	w.CurrentByName(location)
 	return w, nil
+}
+func getForecast5(location, units, lang string) (*owm.Forecast5WeatherData, error) {
+	w, err := owm.NewForecast("5", units, lang, os.Getenv("OWM_API_KEY"))
+	if err != nil {
+		return nil, err
+	}
+	w.DailyByName(location, 5)
+	forecast := w.ForecastWeatherJson.(*owm.Forecast5WeatherData)
+	return forecast, err
 }
 
 func main() {
 	flag.Parse()
 
 	// If there's any funkiness with cli args, tuck and roll...
-	if len(*whereFlag) <= 1 || len(*unitFlag) != 1 || len(*langFlag) != 2 {
+	if len(*whereFlag) <= 1 || len(*unitFlag) != 1 || len(*langFlag) != 2 || len(*whenFlag) <= 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -143,19 +163,34 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Process request for the given location
-	w, err := getCurrent(*whereFlag, *unitFlag, *langFlag)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tmpl, err := template.New("weather").Parse(weatherTemplate)
-	if err != nil {
-		log.Fatalln(err)
+	if *whenFlag == "current" {
+		// Process request for the given location
+		w, err := getCurrent(*whereFlag, *unitFlag, *langFlag)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		tmpl, err := template.New("weather").Parse(weatherTemplate)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Render the template and display
+		if err := tmpl.Execute(os.Stdout, w); err != nil {
+			log.Fatalln(err)
+		}
+	} else { //forecast
+		w, err := getForecast5(*whereFlag, *unitFlag, *langFlag)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		tmpl, err := template.New("forecast").Parse(forecastTemplate)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Render the template and display
+		if err := tmpl.Execute(os.Stdout, w); err != nil {
+			log.Fatalln(err)
+		}
 	}
 
-	// Render the template and display
-	if err = tmpl.Execute(os.Stdout, w); err != nil {
-		log.Fatalln(err)
-	}
 	os.Exit(0)
 }
