d7fffff83ca18ea1b5a3225e90ab1e8dbde16db9
GenDoNL <gendonl@outlook.com>
2019-10-26 17:39:43 +0200

    Add timezone to current struct

--- current.go
+++ current.go
@@ -24,22 +24,23 @@ import (
 // CurrentWeatherData struct contains an aggregate view of the structs
 // defined above for JSON to be unmarshaled into.
 type CurrentWeatherData struct {
-	GeoPos  Coordinates `json:"coord"`
-	Sys     Sys         `json:"sys"`
-	Base    string      `json:"base"`
-	Weather []Weather   `json:"weather"`
-	Main    Main        `json:"main"`
-	Wind    Wind        `json:"wind"`
-	Clouds  Clouds      `json:"clouds"`
-	Rain    Rain        `json:"rain"`
-	Snow    Snow        `json:"snow"`
-	Dt      int         `json:"dt"`
-	ID      int         `json:"id"`
-	Name    string      `json:"name"`
-	Cod     int         `json:"cod"`
-	Unit    string
-	Lang    string
-	Key     string
+	GeoPos   Coordinates `json:"coord"`
+	Sys      Sys         `json:"sys"`
+	Base     string      `json:"base"`
+	Weather  []Weather   `json:"weather"`
+	Main     Main        `json:"main"`
+	Wind     Wind        `json:"wind"`
+	Clouds   Clouds      `json:"clouds"`
+	Rain     Rain        `json:"rain"`
+	Snow     Snow        `json:"snow"`
+	Dt       int         `json:"dt"`
+	ID       int         `json:"id"`
+	Name     string      `json:"name"`
+	Cod      int         `json:"cod"`
+	Timezone int         `json:"timezone"`
+	Unit     string
+	Lang     string
+	Key      string
 	*Settings
 }
 
