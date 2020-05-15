1a33eb40358c77f204e1c93e733b63331c3be567
Kyle Wooten <billykwooten@gmail.com>
2020-05-15 17:05:10 -0400

    Adding feelslike to the main json output

--- openweathermap.go
+++ openweathermap.go
@@ -123,6 +123,7 @@ type Main struct {
 	Temp      float64 `json:"temp"`
 	TempMin   float64 `json:"temp_min"`
 	TempMax   float64 `json:"temp_max"`
+	FeelsLike float64 `json:"feels_like"`
 	Pressure  float64 `json:"pressure"`
 	SeaLevel  float64 `json:"sea_level"`
 	GrndLevel float64 `json:"grnd_level"`
